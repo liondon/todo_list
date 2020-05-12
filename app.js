25b793ba81941afa0609aa2d116fadc868b60f3e
liondon <liondonaugust810810@gmail.com>
2020-05-12 19:53:30 -0400

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-// Initializing Express server
+// Initialize express/mongodb server
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
 
+// set up mongodb connection
+mongoose.connect('mongodb://localhost/todoList', { useNewUrlParser: true, useUnifiedTopology: true })
+// .connect('mongodb://<user>:<password>@<IP addr>:<port>/<dbName>)
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+// set route
 app.get('/', (req, res) => {
   res.send('Hello')
 })

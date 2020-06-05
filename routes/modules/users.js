24c2aa448d51166c14e48033f180e54e17058fc1
liondon <liondonaugust810810@gmail.com>
2020-06-04 20:54:23 -0400

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,4 +5,12 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+  res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 module.exports = router
